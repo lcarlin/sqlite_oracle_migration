fdb3b9ab705405c77b4d89877cd5989763aa6005
Luiz Antonio Carlin - LDA0269 <luiz.carlin@gmail.com>
2026-01-20 10:40:01 -0300

    Ajustes e embelezamento

--- sqlite_oracle_migration.py
+++ sqlite_oracle_migration.py
@@ -1,9 +1,47 @@
 #!/usr/bin/env python3
 """
-SQLite to Oracle Migration Tool - Version 14
-Migrates data from SQLite database to Oracle Database (XE)
-"""
+#############################################################################################
+# Author  : Carlin, Luiz A. .'.
+# e-mail  : luiz.carlin@gmail.com
+# Date    : 16-JAN-2026
+# purpose : Import SQLite3 FULL Database into Oracle Database 
+#           ET&L -> Extract, Transform & Loader
+# Licence : MIT
+#
+# Copyright (c) 2026 Luiz Antonio Carlin
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#############################################################################################
+# Version control
+# Date       #  Version #    What                                      #   Who
+# 2026-01-16 #      1.0 # Merge With Version 6.1 and 7                 # Carlin, Luiz A. .'.
 
+#############################################################################################
+# Current Version : 1.0
+#############################################################################################
+# TODO:
+#############################################################################################
+Dependencies:
+configparser==7.2.0
+cx_Oracle==8.3.0
+
+"""
 import sqlite3
 import configparser
 import sys
